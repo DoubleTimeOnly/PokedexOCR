7d72e7b4880b0c3db16369e8cae1f5ab4a0ce44e
Victor <doubletimeonly@gmail.com>
2021-11-05 17:46:13 -0400

    Add pattern matching module files

---
+++ patternmatching/patmatch.py
@@ -0,0 +1,40 @@
+import cv2
+from screencap.Screen import Screen
+from utils import logger
+
+
+log = logger.get_logger(__name__)
+log.setLevel(logger.DEBUG)
+
+
+class PatternMatcher:
+    def __init__(self, screen_dimensions: tuple):
+        '''
+        :param screen_dimensions: (tuple / list) cornerX, cornerY, width, height
+        '''
+        self.screen = Screen(*screen_dimensions)
+        self.pattern = None
+
+    def find_pattern(self, num_matches=1):
+        matched_patterns = []
+        return matched_patterns
+
+    def load_pattern(self, path_to_pattern):
+        log.debug(f"Reading pattern: {path_to_pattern}")
+        self.pattern = cv2.imread(path_to_pattern, 1)
+        if self.pattern is None:
+            raise FileNotFoundError(f"Could not load file {path_to_pattern}")
+        if log.level <= logger.DEBUG_WITH_IMAGES:
+            cv2.imshow("loaded pattern", self.pattern)
+            cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    screen_dims = (0, 0, 2560, 1440)
+    patmatch = PatternMatcher(screen_dims)
+    if log.level <= logger.DEBUG_WITH_IMAGES:
+        patmatch.screen.showScreen(scale=0.5)
+    patmatch.load_pattern(r"C:\Users\Victor\Documents\OCRPokedex\patterns\HP.PNG")
+    locations = patmatch.find_pattern()
+    print(locations)
+    assert len(locations) == 2
